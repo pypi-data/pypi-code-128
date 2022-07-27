06a426f082896d56b1e35f3513edb1722bb241e6
Tom Forbes <tom@tomforb.es>
2022-07-27 09:18:11 +0000

    {"name":"apachecn-makelove","version":"2022.7.27.0","file":"apachecn_makelove-2022.7.27.0-py3-none-any.whl","path":"apachecn-makelove/apachecn_makelove-2022.7.27.0-py3-none-any.whl"}

---
+++ apachecn-makelove/apachecn_makelove-2022.7.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-makelove
+https://github.com/apachecn/apachecn-makelove"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.27.0"
