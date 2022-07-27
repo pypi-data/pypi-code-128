241919bb0ce71ea3bd6b4ec9e3017d1f5eaea9c9
Tom Forbes <tom@tomforb.es>
2022-07-27 10:20:27 +0000

    {"name":"youqianrende-pianfang","version":"2022.7.27.0","file":"youqianrende-pianfang-2022.7.27.0.tar.gz","path":"youqianrende-pianfang/youqianrende-pianfang-2022.7.27.0.tar.gz"}

---
+++ youqianrende-pianfang/youqianrende-pianfang-2022.7.27.0.tar.gz/YouqianrendePianfang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""youqianrende-pianfang
+https://github.com/apachecn/youqianrende-pianfang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.27.0"
