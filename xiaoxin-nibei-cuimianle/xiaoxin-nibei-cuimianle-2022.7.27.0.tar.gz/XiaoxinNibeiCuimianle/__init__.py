2273f84c65aa4b14f088eb77c51629654e6a51eb
Tom Forbes <tom@tomforb.es>
2022-07-27 10:14:05 +0000

    {"name":"xiaoxin-nibei-cuimianle","version":"2022.7.27.0","file":"xiaoxin-nibei-cuimianle-2022.7.27.0.tar.gz","path":"xiaoxin-nibei-cuimianle/xiaoxin-nibei-cuimianle-2022.7.27.0.tar.gz"}

---
+++ xiaoxin-nibei-cuimianle/xiaoxin-nibei-cuimianle-2022.7.27.0.tar.gz/XiaoxinNibeiCuimianle/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiaoxin-nibei-cuimianle
+https://github.com/apachecn/xiaoxin-nibei-cuimianle"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.27.0"
