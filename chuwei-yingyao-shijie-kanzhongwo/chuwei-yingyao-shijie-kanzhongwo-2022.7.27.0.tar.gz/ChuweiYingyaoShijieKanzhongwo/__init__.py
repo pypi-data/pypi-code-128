9e67f8ab78a7d4842c8d392b8db0303fea5c0923
Tom Forbes <tom@tomforb.es>
2022-07-27 10:00:17 +0000

    {"name":"chuwei-yingyao-shijie-kanzhongwo","version":"2022.7.27.0","file":"chuwei-yingyao-shijie-kanzhongwo-2022.7.27.0.tar.gz","path":"chuwei-yingyao-shijie-kanzhongwo/chuwei-yingyao-shijie-kanzhongwo-2022.7.27.0.tar.gz"}

---
+++ chuwei-yingyao-shijie-kanzhongwo/chuwei-yingyao-shijie-kanzhongwo-2022.7.27.0.tar.gz/ChuweiYingyaoShijieKanzhongwo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chuwei-yingyao-shijie-kanzhongwo
+https://github.com/apachecn/chuwei-yingyao-shijie-kanzhongwo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.27.0"
