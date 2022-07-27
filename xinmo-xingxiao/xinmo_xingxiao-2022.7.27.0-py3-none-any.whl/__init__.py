2b74f3827a4016da66397d8aea4c52a61c98ac20
Tom Forbes <tom@tomforb.es>
2022-07-27 10:15:01 +0000

    {"name":"xinmo-xingxiao","version":"2022.7.27.0","file":"xinmo_xingxiao-2022.7.27.0-py3-none-any.whl","path":"xinmo-xingxiao/xinmo_xingxiao-2022.7.27.0-py3-none-any.whl"}

---
+++ xinmo-xingxiao/xinmo_xingxiao-2022.7.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xinmo-xingxiao
+https://github.com/apachecn/xinmo-xingxiao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.27.0"
