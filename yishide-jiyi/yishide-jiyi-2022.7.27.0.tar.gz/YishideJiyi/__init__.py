41baff2876cc7a67e4b8c9679b34932e31074404
Tom Forbes <tom@tomforb.es>
2022-07-27 10:17:03 +0000

    {"name":"yishide-jiyi","version":"2022.7.27.0","file":"yishide-jiyi-2022.7.27.0.tar.gz","path":"yishide-jiyi/yishide-jiyi-2022.7.27.0.tar.gz"}

---
+++ yishide-jiyi/yishide-jiyi-2022.7.27.0.tar.gz/YishideJiyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yishide-jiyi
+https://github.com/apachecn/yishide-jiyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.27.0"
