aac07c4fd6832a0287b5d31743c0a4a5ff29c028
Tom Forbes <tom@tomforb.es>
2022-07-27 10:18:00 +0000

    {"name":"yiwan-fuweng-mishi-gaobai","version":"2022.7.27.0","file":"yiwan-fuweng-mishi-gaobai-2022.7.27.0.tar.gz","path":"yiwan-fuweng-mishi-gaobai/yiwan-fuweng-mishi-gaobai-2022.7.27.0.tar.gz"}

---
+++ yiwan-fuweng-mishi-gaobai/yiwan-fuweng-mishi-gaobai-2022.7.27.0.tar.gz/YiwanFuwengMishiGaobai/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yiwan-fuweng-mishi-gaobai
+https://github.com/apachecn/yiwan-fuweng-mishi-gaobai"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.27.0"
